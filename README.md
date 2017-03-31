0d1549fb94585ef9f3ff3d7b90cbead1128ff6dd
Paul Lamere <Paul.Lamere@gmail.com>
2017-03-31 17:13:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+See the demo at http://static.echonest.com/NowPlayingOnSpotify/
---
+++ config.js
@@ -0,0 +1,5 @@
+"use strict";
+var SPOTIFY_CLIENT_ID = '1888c6110f814565bb273b29b0f88c58';
+var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/NowPlayingOnSpotify/'
+var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+var SPOTIFY_REDIRECT_URI = REMOTE_SPOTIFY_REDIRECT_URI;
